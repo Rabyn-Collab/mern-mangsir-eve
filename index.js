5e0df8cc15ceee8a21d96bb057967225662acca0
Rabyn-Collab <babynshrestha64@gmail.com>
2026-01-23 16:03:26 +0545

    changes

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+const handleInput = () => {
+  const inputElement = document.querySelector('input');
+  inputElement.focus();
+
+}
+
+const func = () => {
+  const h1Element = document.querySelector('#sec-3');
+  h1Element.scrollIntoView({
+    behavior: 'smooth',
+
+  });
+}
