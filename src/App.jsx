0ee6d88f1c4658395fcbb07cb162b4ba96491aad
Rabyn-Collab <babynshrestha64@gmail.com>
2026-02-24 16:15:16 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import NotFound from "./pages/not-found/NotFound.jsx";
 import RootLayout from "./components/RootLayout.jsx";
-import ItemList from "./pages/meals/ItemList.jsx";
-import Meal from "./pages/meals/Meal.jsx";
-import UseParams from "./pages/params/UseParams.jsx";
-import UseSearchParams from "./pages/params/UseSearchParams.jsx";
-import Search from "./pages/meals/Search.jsx";
+
 
 
 export default function App() {
@@ -23,34 +19,6 @@ export default function App() {
           element: <Home />,
 
         },
-
-        {
-          path: 'items-list/:label',
-          element: <ItemList />,
-        },
-
-        {
-          path: 'meal/:id',
-          element: <Meal />
-
-        },
-
-        {
-          path: 'search',
-          element: <Search />
-        },
-
-        {
-          path: 'use-params/:id',
-          element: <UseParams />
-        },
-
-        {
-          path: 'searchParams',
-          element: <UseSearchParams />
-
-        },
-
 
 
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,38 +1,9 @@
-import { useNavigate } from "react-router";
-import { Input } from "../../components/ui/input.jsx";
-import MealCategoryList from "../meals/MealCategoryList.jsx";
+
 
 export default function Home() {
-
-
-  const nav = useNavigate();
-
   return (
     <div>
-      <div className="grid grid-cols-4 items-center">
-        <img src="https://www.themealdb.com/images/meal-icon.png" alt="" />
 
-        <div className="text-white text-center col-span-2 space-y-3">
-          <h1>Welcome to TheMealDB</h1>
-          <p>Welcome to TheMealDB: An open, crowd-sourced database of recipes from around the world.
-            We offer a free recipe API for anyone wanting to use it, with additional premium features if required.</p>
-        </div>
-
-        <img src="https://www.themealdb.com/images/meal-icon.png" alt="" />
-      </div>
-
-      <div className="flex justify-center mb-9">
-        <form action={(formData) => {
-          nav(`/search?s=${formData.get('search')}`);
-        }} className="max-w-2xl">
-          <Input
-            name="search"
-            className="w-96 inline-block bg-white"
-            type="text" placeholder="Search" />
-
-        </form>
-      </div>
-      <MealCategoryList />
 
     </div>
   )
