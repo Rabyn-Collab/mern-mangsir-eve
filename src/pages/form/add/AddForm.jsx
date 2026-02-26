7fed0a644e5d6922feaac244059412c0b252caaf
Rabyn-Collab <babynshrestha64@gmail.com>
2026-02-26 16:53:07 +0545

    changes

--- src/pages/form/add/AddForm.jsx
+++ src/pages/form/add/AddForm.jsx
@@ -9,9 +9,41 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Formik } from "formik"
+import { RadioGroup, RadioGroupItem } from "../../../components/ui/radio-group.jsx"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../../../components/ui/select.jsx"
+import { Textarea } from "../../../components/ui/textarea.jsx"
+import * as Yup from "yup";
+import { useState } from "react"
+
+
+const valSchema = Yup.object({
+  username: Yup.string().min(4).max(20).required('username is required'),
+  email: Yup.string().email().required(),
+  gender: Yup.string().required(),
+  country: Yup.string().required(),
+  detail: Yup.string().min(10).max(500).required(),
+  // image: Yup
+  //   .mixed()
+  //   .test("fileSize", "The file is too large", (value) => value && value.size <= 2 * 1024 * 1024)
+  //   .test('fileType', 'Unsupported file type', (val) => {
+  //     return val && ['image/jpeg', 'image/png', 'image/jpg', 'image/webp', 'image/gif'].includes(val.type);
+  //   })
+  //   .required()
+
+});
+
+
 export default function AddForm() {
+
+  const [data, setData] = useState([]);
+
+  console.log(data);
+
+
   return (
     <div>
+
+
       <Card className="w-full max-w-sm">
         <CardHeader>
           <CardTitle>Add Some</CardTitle>
@@ -27,13 +59,22 @@ export default function AddForm() {
 
           <Formik
             initialValues={{
-              email: ''
+              username: '',
+              email: '',
+              gender: '',
+              country: '',
+              detail: '',
+              // image: '',
+              // imageReview: ''
             }}
-            onSubmit={(val) => {
+            onSubmit={(val, { resetForm }) => {
 
-              console.log(val);
+              setData([...data, val]);
+              resetForm();
 
             }}
+
+            validationSchema={valSchema}
 
           >
 
@@ -43,6 +84,24 @@ export default function AddForm() {
                 onSubmit={handleSubmit}
               >
                 <div className="flex flex-col gap-6">
+
+
+                  <div className="grid gap-2">
+                    <Label htmlFor="username">Username</Label>
+
+                    <Input
+                      name='username'
+                      value={values.username}
+                      onChange={handleChange}
+                      id="username"
+                      type="text"
+                      placeholder="John Doe"
+
+                    />
+                    {errors.username && touched.username && <p className="text-red-500">{errors.username}</p>}
+                  </div>
+
+
                   <div className="grid gap-2">
                     <Label htmlFor="email">Email</Label>
 
@@ -55,7 +114,92 @@ export default function AddForm() {
                       placeholder="m@example.com"
 
                     />
+                    {errors.email && touched.email && <p className="text-red-500">{errors.email}</p>}
                   </div>
+
+                  <div>
+                    <h4>Select Your Gender</h4>
+
+                    <RadioGroup
+                      onValueChange={(val) => {
+                        setFieldValue('gender', val);
+                      }}
+                      defaultValue="comfortable" className="w-fit mt-3">
+                      <div className="flex items-center gap-3">
+                        <RadioGroupItem value="male" id="r1" />
+                        <Label htmlFor="r1">Male</Label>
+                      </div>
+                      <div className="flex items-center gap-3">
+                        <RadioGroupItem value="female" id="r2" />
+                        <Label htmlFor="r2">Female</Label>
+                      </div>
+
+                    </RadioGroup>
+                    {errors.gender && touched.gender && <p className="text-red-500">{errors.gender}</p>}
+
+
+
+                  </div>
+
+                  <div>
+                    <h4>Select Your Country</h4>
+                    <Select
+                      onValueChange={(val) => {
+                        setFieldValue('country', val);
+                      }}
+
+                    >
+                      <SelectTrigger className="w-full max-w-48 mt-2">
+                        <SelectValue placeholder="Select a country" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+
+                          <SelectItem value="nepal">Nepal</SelectItem>
+                          <SelectItem value="india">India</SelectItem>
+                          <SelectItem value="china">China</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                    {errors.country && touched.country && <p className="text-red-500">{errors.country}</p>}
+                  </div>
+
+                  <div className="grid gap-2">
+                    <Label htmlFor="detail">Detail</Label>
+
+                    <Textarea
+                      name='detail'
+                      value={values.detail}
+                      onChange={handleChange}
+                      id="detail"
+                      type="text"
+                      placeholder="details"
+
+                    />
+                    {errors.detail && touched.detail && <p className="text-red-500">{errors.detail}</p>}
+                  </div>
+
+
+                  {/* <div className="grid gap-2">
+                    <Label htmlFor="image">Select an Image</Label>
+
+                    <Input
+                      name='image'
+                      onChange={(e) => {
+                        const file = e.target.files[0];
+                        setFieldValue('imageReview', URL.createObjectURL(file));
+                        setFieldValue('image', file);
+                      }}
+                      type="file"
+                    />
+
+                    {values.imageReview && !errors.image && <img src={values.imageReview} alt="" className="w-64 h-48" />}
+                    {errors.image && touched.image && <p className="text-red-500">{errors.image}</p>}
+                  </div> */}
+
+
+
+
 
                 </div>
                 <Button type="submit" className="w-full mt-5">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,14 @@
 
-
 export default function Home() {
+
+
+  // const numbers = [11, 22, 33, 44, 55];
+
+  // console.log([...numbers, 99, 100]);
+
+
+  const persons = ['ram', 'shyam', 'hari', 'rita'];
+
   return (
     <div>
 
