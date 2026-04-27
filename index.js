6b36db78ab49eb57452dd556ab2c04426febb6b7
Rabyn-Collab <babynshrestha64@gmail.com>
2026-04-27 19:34:30 +0545

    changes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(fileUpload({
 
 app.use(express.static('uploads'));
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://mern-mangsir-eve.vercel.app'],
 }));
 
 
