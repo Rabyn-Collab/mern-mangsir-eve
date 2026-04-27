b8133161860737f53d7cb2161b607e86cbd30cf5
Rabyn-Collab <babynshrestha64@gmail.com>
2026-04-27 19:45:30 +0545

    changes

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-
+//
 
 //export const base = 'http://192.168.1.65:5000';
 export const base = 'https://mern-mangsir-eve.onrender.com';
